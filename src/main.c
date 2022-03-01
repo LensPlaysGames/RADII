3639365bb494a07228c02cd1e7bc25090c54faa2
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-01 13:58:07 -0800

    Hello, World!

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "EFI/system_table.h"
 
 EFI_STATUS efi_main(EFI_HANDLE *imageHandle, EFI_SYSTEM_TABLE *systemTable) {
+  systemTable->ConsoleOut->OutputString(systemTable->ConsoleOut, L"Hello, World!\r\n");
   (VOID)imageHandle;
   (VOID)systemTable;
   return 420;
