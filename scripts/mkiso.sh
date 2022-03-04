aab21b41a594cb71c0d6f14442b274cbdfae398c
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-03 16:01:32 -0800

    Harden `mkiso.sh`
    It was rather flimsy when run from any directory other than the `bin` directory, which has now been fixed.

--- scripts/mkiso.sh
+++ scripts/mkiso.sh
@@ -1,6 +1,6 @@
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $ScriptDirectory/mkimg.sh
-mkdir -p iso
-cp test-build.img iso
-xorriso -as mkisofs -R -f -e test-build.img -no-emul-boot -o test-build.iso iso
-rm -r iso
+mkdir -p $ScriptDirectory/iso
+cp $ScriptDirectory/../bin/test-build.img $ScriptDirectory/iso
+xorriso -as mkisofs -R -f -e test-build.img -no-emul-boot -o $ScriptDirectory/../bin/test-build.iso $ScriptDirectory/iso
+rm -r $ScriptDirectory/iso
