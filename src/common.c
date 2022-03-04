788345c018d499ca675b57762264aac8243d3722
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-03 19:26:08 -0800

    Initialize `MemoryInfo` to the EFI Memory Map

--- src/common.c
+++ src/common.c
@@ -1,10 +1,20 @@
 #include "common.h"
 
+#include "boot_information.h"
+#include <efi.h>
+
 EFI_SYSTEM_TABLE *SystemTable;
 EFI_HANDLE *ImageHandle;
+
+MemoryInformation MemoryInfo;
 
 EFI_STATUS Initialize(EFI_SYSTEM_TABLE *ST, EFI_HANDLE *IH) {
   SystemTable = ST;
   ImageHandle = IH;
+
+  SystemTable->BootServices->GetMemoryMap(&MemoryInfo.MapSizeInBytes, MemoryInfo.Map, &MemoryInfo.MapKey, &MemoryInfo.DescriptorSize, &MemoryInfo.DescriptorVersion);
+  SystemTable->BootServices->AllocatePool(LOADER_DATA, MemoryInfo.MapSizeInBytes, (void**)&MemoryInfo.Map);
+  SystemTable->BootServices->GetMemoryMap(&MemoryInfo.MapSizeInBytes, MemoryInfo.Map, &MemoryInfo.MapKey, &MemoryInfo.DescriptorSize, &MemoryInfo.DescriptorVersion);
+
   return EFI_SUCCESS;
 }
--- src/common.h
+++ src/common.h
@@ -1,10 +1,13 @@
 #ifndef RADII_BOOTLOADER_COMMON_H
 #define RADII_BOOTLOADER_COMMON_H
 
+#include "boot_information.h"
 #include <efi.h>
 
 extern EFI_SYSTEM_TABLE *SystemTable;
 extern EFI_HANDLE *ImageHandle;
+
+extern MemoryInformation MemoryInfo;
 
 EFI_STATUS Initialize(EFI_SYSTEM_TABLE *ST, EFI_HANDLE *IH);
 
