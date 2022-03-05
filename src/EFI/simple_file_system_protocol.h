c1a871cf5bd759232f702bc53cd472923ba42cd4
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-04 16:41:35 -0800

    Header alphabetization

--- src/EFI/simple_file_system_protocol.h
+++ src/EFI/simple_file_system_protocol.h
@@ -1,8 +1,8 @@
 #ifndef RADII_BOOTLOADER_SIMPLE_FILE_SYSTEM_PROTOCOL_H
 #define RADII_BOOTLOADER_SIMPLE_FILE_SYSTEM_PROTOCOL_H
 
-#include "types.h"
 #include "file_protocol.h"
+#include "types.h"
 
 #define EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID \
  {0x0964e5b22,0x6459,0x11d2, {0x8e,0x39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
