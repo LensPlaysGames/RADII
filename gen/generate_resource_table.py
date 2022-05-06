479f3c8d3b4b20b25ac6219386e5cd97c9391b7a
Lens <LensPlaysGames@gmail.com>
2022-05-06 12:56:02 -0700

    Very basic, very WIP resource table generator
    The beginning of the rabbit-hole has made itself known...
    
    As for `table.json`, it is temporary, but a good example of how the
    bootloader will be human-configurable. Most likely, the resource table
    will be just one configurable part, and will move into a larger
    `config.json` in the root of the repository.
    
    It would be good to be able to include files loaded by UEFI services
    at runtime as well (to reduce bootloader executable bloat, and more).
    This feature is planned, but the implementation is still fuzzy. I'll
    give it some thought as I work on the simple resource table
    include-file-in-executable style.

---
+++ gen/generate_resource_table.py
@@ -0,0 +1,72 @@
+from os.path import exists
+import json
+
+# TODO:
+# |-- Load JSON, XAML, YAML, TOML, INI, or some format to parse the resource table header definitions.
+# `-- Use Resource Table Headers data to generate a C definition using templates with replaceable text.
+
+# Also just thought of the fact that these resources will be embedded in the bootloader executable.
+#     This has it's advantages and disadvantages; we'll see how exactly it pans out.
+#     It may be necessary to be able to specify run-time files to find and load using UEFI,
+#     but that is a whoooole can-o-worms I don't want to even think about right now.
+
+def main():
+    # Look for `table.json`
+    if not exists("table.json"):
+        print("Could not find \"table.json\", exiting.")
+        exit(1)
+        
+    resource_table_file = open("table.json", "rt")
+    resource_table_json = json.loads(resource_table_file.read())
+
+    if not resource_table_json["resource-table"]:
+        print("Could not find \"resource-table\" entry in JSON.")
+        exit(1)
+
+    premade = [
+        "memory-map",
+        "gfx",
+        "acpi",
+    ]
+
+    resource_table_json = resource_table_json["resource-table"]
+    for key, value in resource_table_json.items():
+        # TODO:
+        # `-- Create list of resource table entries, with metadata.
+        #     |-- Check for pre-defined entries.
+        #     `-- Parse user-created entries.
+        if key in premade:
+            print("found recognized pre-made resource table entry:", key)
+        else:
+            # Parse custom resource table entry
+            # Ensure valid signature
+            if not value["signature"]:
+                print("Table entry definition missing signature:", key)
+
+            if not len(value["signature"]) == 16:
+                print("Table entry definition has invalid-sized signature:", key)
+                print("  The signature should be 16 bytes long")
+                exit(1)
+
+            # Ensure valid contents
+            if not value["contents"]:
+                print("Table entry definition missing contents:", key)
+
+            try:
+               byteCount = int(value["contents"])
+               print("Found header with empty byte allocation:", key)
+            except ValueError:
+                # TODO:
+                # |-- Check if file at value["contents"] exists, get size to allocate in declaration.
+                # `-- Somehow pass file contents on to be able to be used for definition.
+                if not exists(value["contents"]):
+                    print("Could not find file at \"", value["contents"],"\", contents of", key)
+                print("Found header with file path as contents:", key)
+
+            pass
+
+    resource_table_file.close()
+    
+
+if __name__ == '__main__':
+    main()
