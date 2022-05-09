2898b7ed25238ae950774f5c98dfc403dc11b0df
Lens <LensPlaysGames@gmail.com>
2022-05-08 18:06:33 -0700

    Allow forceful generation in `generate_resource_table.cmake`
    Just pass `-DRADII_FORCE_GENERATION=1`

--- generate_resource_table.cmake
+++ generate_resource_table.cmake
@@ -1,31 +1,33 @@
 find_program(PYTHON_PROGRAM python python3)
 if(PYTHON_PROGRAM)
-  if(NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/src/resource_table.h
-	  OR NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/src/resource_table.c)
-	if(NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/gen/resource_table.h
-		OR NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/gen/resource_table.c)
-	  message(STATUS "Generating resource table")
-	  execute_process(
-		COMMAND
-		${PYTHON_PROGRAM}
-		generate_resource_table.py
-		--config ${CONFIG_FILE}
-		WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/gen
-		COMMAND_ERROR_IS_FATAL ANY
-	  )
-	endif()
-	message(STATUS "Copying resource table from gen/ to src/")
-	execute_process(
-	  COMMAND ${CMAKE_COMMAND} -E copy gen/resource_table.h src/
-	  COMMAND ${CMAKE_COMMAND} -E copy gen/resource_table.c src/
-	  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-	  COMMAND_ERROR_IS_FATAL ANY
-	)
+  if(DEFINED RADII_FORCE_GENERATION
+      OR NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/src/resource_table.h
+      OR NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/src/resource_table.c)
+    if(DEFINED RADII_FORCE_GENERATION
+        OR NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/gen/resource_table.h
+        OR NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/gen/resource_table.c)
+      message(STATUS "Generating resource table")
+      execute_process(
+        COMMAND
+        ${PYTHON_PROGRAM}
+        generate_resource_table.py
+        --config ${CONFIG_FILE}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/gen
+        COMMAND_ERROR_IS_FATAL ANY
+      )
+    endif()
+    message(STATUS "Copying resource table from gen/ to src/")
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E copy gen/resource_table.h src/
+      COMMAND ${CMAKE_COMMAND} -E copy gen/resource_table.c src/
+      WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+      COMMAND_ERROR_IS_FATAL ANY
+    )
   endif()
 else()
   message(
-	FATAL_ERROR
-	"Could not find python on system; it is necessary to generate a resource table.
+    FATAL_ERROR
+    "Could not find python on system; it is necessary to generate a resource table.
 https://www.python.org/downloads/"
   )
 endif()
